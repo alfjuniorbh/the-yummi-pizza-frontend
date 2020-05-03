56c2ac6f718ef755749e02a0205b54ffa3058f6b
Adelino Masioli <alfjuniorbh.web@gmail.com>
2020-05-03 22:27:47 +0100

    The Yummi Pizza build

---
+++ build/precache-manifest.55c709b7fc4dd2468b11800373b89a75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e0b806ffffca7b683d1b0b3f232ea5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf6baf51791a17c3a9ae",
+    "url": "/static/js/2.9bab3a44.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.9bab3a44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "183b609b918a20ff2251",
+    "url": "/static/js/main.7c9c6131.chunk.js"
+  },
+  {
+    "revision": "4e922de799813fe852b7",
+    "url": "/static/js/runtime-main.786b7902.js"
+  },
+  {
+    "revision": "777af13826537cd822e2d266dbd93e62",
+    "url": "/static/media/logo.777af138.svg"
+  }
+]);
